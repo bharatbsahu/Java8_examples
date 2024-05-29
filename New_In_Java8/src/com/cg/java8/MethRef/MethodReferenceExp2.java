37a53cc144a14c022a56ef6e0e6b06e176399ea3
bharatbsahu <smartbharat@gmail.com>
2024-05-29 17:13:09 +0530

    Create MethodReferenceExp2.java

---
+++ New_In_Java8/src/com/cg/java8/MethRef/MethodReferenceExp2.java
@@ -0,0 +1,28 @@
+package com.cg.java8.MethRef;
+
+@FunctionalInterface
+interface Sayable1{  
+    void say();  
+}  
+
+
+public class MethodReferenceExp2 {  
+	
+	
+    public void saySomething(){  
+        System.out.println("Hello, this is non-static method.");  
+    }  
+    
+    
+    public static void main(String[] args) {  
+    	
+    	MethodReferenceExp2 methExp2 = new MethodReferenceExp2();
+    	
+    	Sayable1 say1 = methExp2::saySomething;
+    	say1.say();
+    	
+    	
+    	
+    	
+    }
+}
